2271680da5ec0caaa0f1baaae037296d0570e1cb
Jon <lundbyjon@hotmail.com>
2024-07-17 03:58:17 +0200

    added some test db disciplines to data.sql file + app.properties setup to alternate between create and update db

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -8,7 +8,9 @@ USE exam24h;
 -- DISCIPLINES
     INSERT INTO disciplines (name, result_type) VALUES ("100m-løb", "time");
     INSERT INTO disciplines (name, result_type) VALUES ("Diskoskast", "length");
-    INSERT INTO disciplines (name, result_type) VALUES ("Tre-spring", "length");
+    INSERT INTO disciplines (name, result_type) VALUES ("Trespring", "length");
+    INSERT INTO disciplines (name, result_type) VALUES ("Højdespring", "length");
+    INSERT INTO disciplines (name, result_type) VALUES ("Spydkast", "length");
 
 -- RESULTS
     INSERT INTO results (result_type, result_value, participant_id, discipline_id) VALUES ("time", "00.09.58", 1, 1);
